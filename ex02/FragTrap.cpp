27118a2483a7ca6a9857901df90d21235147c1cc
TomTris <quoctrido.jb@gmail.com>
2024-06-03 19:58:40 +0200

    ex02

--- ex02/FragTrap.cpp
+++ ex02/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 16:44:54 by qdo               #+#    #+#             */
-/*   Updated: 2024/06/03 17:06:26 by qdo              ###   ########.fr       */
+/*   Updated: 2024/06/03 19:24:38 by qdo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 FragTrap::FragTrap() : ClapTrap()
 {
 	std::cout << "FrapTrap" << getName() << std::endl;
+	this->setName("Default");
 	this->setHP(100);
 	this->setMana(100);
 	this->setDamage(30);
@@ -32,9 +33,9 @@ FragTrap::FragTrap(FragTrap const &src)
 {
 	std::cout << "ScavTrap Copy Constructor called" << std::endl;
 	this->setName(src.getName());
-	this->setHP(100);
-	this->setMana(100);
-	this->setDamage(30);
+	this->setHP(src.getHP());
+	this->setMana(src.getHP());
+	this->setDamage(src.getHP());
 }
 FragTrap::~FragTrap()
 {
