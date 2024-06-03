c62d10f45489b05dadfdc499bc134ccf6526b573
TomTris <quoctrido.jb@gmail.com>
2024-06-03 17:09:26 +0200

    ex02

---
+++ ex02/FragTrap.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 16:44:53 by qdo               #+#    #+#             */
+/*   Updated: 2024/06/03 16:56:27 by qdo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap
+{
+
+private:
+
+public:
+	FragTrap();
+	FragTrap(std::string name);
+	FragTrap(FragTrap const &src);
+	~FragTrap();
+
+	FragTrap & operator=(FragTrap const &src);
+	void highFiveGuys( void );
+};
+
+
+
+#endif
