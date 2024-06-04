a973de3254062060643ad95f3219d549338a4d80
Quoc Tri Do <qdo@1-C-26.42heilbronn.de>
2024-06-04 15:57:49 +0200

    ex01

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -5,71 +5,61 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/02 12:28:30 by qdo               #+#    #+#             */
-/*   Updated: 2024/06/03 16:55:35 by qdo              ###   ########.fr       */
+/*   Created: 2024/06/04 15:54:20 by qdo               #+#    #+#             */
+/*   Updated: 2024/06/04 15:55:10 by qdo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
+ScavTrap::~ScavTrap()
+{
+	std::cout << "ScavTrap " << _name << " now left" << std::endl;
+}
 ScavTrap::ScavTrap() : ClapTrap()
 {
-	std::cout << "ScavTrap Default was born" << std::endl;
-	this->setName("Default");
-	this->setHP(100);
-	this->setMana(50);
-	this->setDamage(20);
-	this->_guarding_gate = false;
+	_name = "Default";
+	std::cout << "ScavTrap " << _name << " was born" << std::endl;
+	_guard = false;
+	_HP = 100;
+	_EP = 50;
+	_AD = 20;
 }
 
 ScavTrap::ScavTrap(std::string name) : ClapTrap(name)
 {
-	std::cout << "ScavTrap " << name << " was born" << std::endl;
-	this->setName(name);
-	this->setHP(100);
-	this->setMana(50);
-	this->setDamage(20);
-	this->_guarding_gate = false;
+	_name = name;
+	std::cout << "ScavTrap " << _name << " was born" << std::endl;
+	_guard = false;
+	_HP = 100;
+	_EP = 50;
+	_AD = 20;
 }
 
-ScavTrap::~ScavTrap()
-{
-	std::cout << "ScavTrap " << getName() << " now left" << std::endl;
-}
-
-ScavTrap & ScavTrap::operator=(ScavTrap const & src)
+ScavTrap::ScavTrap(ScavTrap & src)
 {
 	std::cout << "ScavTrap Copy Constructor called" << std::endl;
 	if (this != &src)
-	{
-		this->setName(src.getName());
-		this->setHP(src.getHP());
-		this->setMana(src.getMana());
-		this->setDamage(src.getDamage());
-	}
+		*this = src;
+}
+
+ScavTrap& ScavTrap::operator=(ScavTrap &src)
+{
+	std::cout << "ScavTrap Copy Assignation called" << std::endl;
+	_name = src._name;
+	_HP = src._HP;
+	_EP = src._EP;
+	_AD = src._AD;
 	return (*this);
 }
 
-void	ScavTrap::guardGate()
+void ScavTrap::guardGate(void)
 {
-	if (this->_guarding_gate == false)
+	if (_guard == false)
 	{
-		std::cout << this->getName() << " now becomes GuardGate" << std::endl;
-		this->_guarding_gate = true;
+		_guard = true;
+		std::cout << "ScavTrap " << _name << " is now GuardGate" << std::endl;
+		return;
 	}
-	else
-		std::cout << this->getName() << " is already Guardgate" << std::endl;
-}
-
-void ScavTrap::attack(const std::string & name)
-{
-	if (this->getHP() == 0)
-		std::cout << "ScavTrap " << getName() << ": \033[33mcan't attack anymore because he's dead already\033[0m" << std::endl;
-	else if (this->getMana() == 0)
-		std::cout << "ScavTrap " << getName() << ": \033[33mcan't attack anymore because he doesn't have enough Energy Points left\033[0m" << std::endl;
-	else
-	{
-		this->setMana(this->getMana() - 1);
-		std::cout << "ScavTrap " << getName() << " attacks " << name << ", causing " << getDamage() << " points of damage!" << std::endl;
-	}
+	std::cout << "ScavTrap " << _name << " is already GuardGate" << std::endl;
 }
