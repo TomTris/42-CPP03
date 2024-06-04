d92106bf7bcbb7d58dc7b8d8be9e3837ab77329e
Quoc Tri Do <qdo@2-I-5.42heilbronn.de>
2024-06-04 18:50:19 +0200

    CPP03

---
+++ ex03/ClapTrap.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 14:52:56 by qdo               #+#    #+#             */
+/*   Updated: 2024/06/04 18:34:41 by qdo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap {
+	protected:
+		std::string _name;
+		unsigned int _HP;
+		unsigned int _EP;
+		unsigned int _AD;
+
+	public:
+		~ClapTrap();
+		ClapTrap();
+		ClapTrap(std::string name);
+		ClapTrap(ClapTrap & src);
+		ClapTrap & operator=(ClapTrap & src);
+		
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+};
+
+
+#endif
